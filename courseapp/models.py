41658e1747ab013b93e3078e3efab61705ba5f21
Ankit Shukla <premusha12345@gmail.com>
2024-04-11 23:58:10 +0530

    new stuff

--- courseapp/models.py
+++ courseapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Lesson(models.Model):
+    LEVEL_CHOICES = [
+        ('B', 'Beginner'),
+        ('I', 'Intermediate'),
+        ('A', 'Advanced'),
+    ]
+
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    points = models.IntegerField()
+    level = models.CharField(max_length=1, choices=LEVEL_CHOICES)
+
+    def __str__(self):
+        return self.name
