a7a00b4de4523cb845b879e5608c166cf36b0bb5
Ankit Shukla <premusha12345@gmail.com>
2024-04-27 01:11:30 +0530

    url work

--- courseapp/urls.py
+++ courseapp/urls.py
@@ -10,11 +10,6 @@ urlpatterns = [
 ]
 
 api_urlpatterns = [
-    path('lesson', views.LessonList.as_view(), name='lesson_list'),
-    path('lesson/<int:pk>', views.LessonDetail.as_view(), name='lesson_detail'),
-    path('badge', views.BadgeList.as_view(), name='badge_list'),
-    path('badge/<int:pk>', views.BadgeDetail.as_view(), name='badge_detail'),
-    path('quiz', views.QuizList.as_view(), name='quiz_list'),
-    path('quiz/<int:pk>', views.QuizDetail.as_view(), name='quiz_detail'),
-
+    
+   
 ]
